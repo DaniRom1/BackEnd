55712129c153b96f0b576b1cd735cf30b908e2a3
Nicholas <nicholaszgp@hotmail.com>
2023-10-18 14:02:34 +0200

    commit

--- routes/api.php
+++ routes/api.php
@@ -27,25 +27,31 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('login', \App\Http\Controllers\Restify\Auth\LoginController::class)
     ->middleware('throttle:6,1')
     ->name('restify.login');
-Route::post('register', \App\Http\Controllers\Restify\Auth\RegisterController::class)
+
+    Route::post('register', \App\Http\Controllers\Restify\Auth\RegisterController::class)
     ->name('restify.register');
-Route::post('forgotPassword', \App\Http\Controllers\Restify\Auth\ForgotPasswordController::class)
+
+    Route::post('forgotPassword', \App\Http\Controllers\Restify\Auth\ForgotPasswordController::class)
     ->middleware('throttle:6,1')
     ->name('restify.forgotPassword');
-Route::post('forgotPassword', \App\Http\Controllers\Restify\Auth\ForgotPasswordController::class)
+
+    Route::post('forgotPassword', \App\Http\Controllers\Restify\Auth\ForgotPasswordController::class)
     ->middleware('throttle:6,1')
     ->name('restify.forgotPassword');
-Route::post('forgotPassword', \App\Http\Controllers\Restify\Auth\ForgotPasswordController::class)
+
+    Route::post('forgotPassword', \App\Http\Controllers\Restify\Auth\ForgotPasswordController::class)
     ->middleware('throttle:6,1')
     ->name('restify.forgotPassword');
-Route::post('resetPassword', \App\Http\Controllers\Restify\Auth\ResetPasswordController::class)
+
+    Route::post('resetPassword', \App\Http\Controllers\Restify\Auth\ResetPasswordController::class)
     ->middleware('throttle:6,1')
     ->name('restify.resetPassword');
-Route::post('verify/{id}/{hash}', \App\Http\Controllers\Restify\Auth\VerifyController::class)
+
+    Route::post('verify/{id}/{hash}', \App\Http\Controllers\Restify\Auth\VerifyController::class)
     ->middleware('throttle:6,1')
     ->name('restify.verify');
 
-Route::post('verify/{id}/{hash}', \App\Http\Controllers\Restify\Auth\VerifyController::class)
+    Route::post('verify/{id}/{hash}', \App\Http\Controllers\Restify\Auth\VerifyController::class)
     ->middleware('throttle:6,1')
     ->name('restify.verify');
 
